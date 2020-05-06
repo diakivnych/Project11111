9816a71a7eb3aca2e00f6e2b33441047dac4a201
Dima <d.diakivnych@gmail.com>
2020-05-07 02:58:40 +0300

    make it better

--- project1/settings.py
+++ project1/settings.py
@@ -128,8 +128,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    'project1/project1/apps/students/static',
-    'project1/apps/students/static',
-    'apps/students/static',
-    'static'
+    'apps/students/static'
 ]
