5a0b5e7d1dfddddc22991a499fce2f52606bd2d1
Dima <d.diakivnych@gmail.com>
2020-05-07 02:59:24 +0300

    make it better

--- project1/settings.py
+++ project1/settings.py
@@ -126,7 +126,3 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    'apps/students/static'
-]
